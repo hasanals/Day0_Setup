28ce2176763f85a195e6a64ab26d5f0ad664928b
HisDesigns <h.i.i.s@hotmail.com>
2020-05-01 11:55:23 +0300

    Initial Commit

---
+++ Day0_Setup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day0_Setup
+//
+//  Created by Hasan Alsaffar on 5/1/20.
+//  Copyright © 2020 Hasan Alsaffar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
